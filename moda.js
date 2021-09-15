aabf21e6557008ac35e9b55e9b4e8610d91fee12
juan cortes <jcamilocortesbespoke@gmail.com>
2021-09-15 07:36:04 +0100

    se adjunta el promedio ponderado para sacar un promedio de notas de estudio

--- moda.js
+++ moda.js
@@ -29,3 +29,47 @@ const lista1Array = Object.entries(lista1Count).sort(
 );
 
 const moda = lista1Array[lista1Array.length - 1];
+
+//Promedio Ponderado para sacar la media de un estudiante de sus notas
+
+const notes = [
+  {
+    course: "aviation",
+    note: 10,
+    credit: 2,
+  },
+  {
+    course: "programing",
+    note: 8,
+    credit: 5,
+  },
+  {
+    course: "AI",
+    note: 7,
+    credit: 5,
+  },
+];
+// Aqui multiplicamos cada nota con sus creditos
+const notesWithCredit = notes.map(function(noteObject) {
+  return noteObject.note * noteObject.credit;
+});
+
+// Aqui sumamos todos los elementos de la multiplicacion
+
+const sumOfNotesWithCredit = notesWithCredit.reduce(
+  function(sum = 0, newVal) {
+    return sum + newVal;
+  }
+);
+// Sumar todos los pesos (creditos)
+const credits = notes.map(function(noteObject) {
+  return noteObject.credit
+});
+const sumOfCredits = credits.reduce(
+  function (sum = 0,newVal) {
+    return sum + newVal;
+  }
+);
+
+// Dividir ambas variables (sumas)
+const promedio promedioPonderadoNotasConCreditos = sumOfNotesWithCredit / sumOfCredits;
